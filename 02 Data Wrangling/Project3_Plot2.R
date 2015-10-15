175a6f65836de6f3807c0758342c5281f3ba25a2
Spencer Muncey <scmuncey@yahoo.com>
2015-10-14 23:42:09 -0500

    Changed Plot1_df to Plot2_df and Plot3_df respectively

--- 02 Data Wrangling/Project3_Plot2.R
+++ 02 Data Wrangling/Project3_Plot2.R
@@ -3,19 +3,19 @@ require(tidyr)
 require(dplyr)
 require(ggplot2)
 # Join the fast food and zip code data into new data frame for Plot 1
-Plot1_df <- dplyr::left_join(fast_food, zip_code, by="ZIP") 
+Plot2_df <- dplyr::left_join(fast_food, zip_code, by="ZIP") 
 
 # Need to relabel the data in the RESTAURANT column
-Plot1_df$RESTAURANT <- factor(Plot1_df$RESTAURANT, levels=c("m", "b", "p", "t","w", "j", "h", "c", "i", "k"), labels = c("McDonalds", "Burger King", "Pizza Hut", "Taco Bell", "Wendys", "Jack In The Box", "Hardees", "Carls Jr.", "In-N-Out", "KFC")) 
+Plot2_df$RESTAURANT <- factor(Plot2_df$RESTAURANT, levels=c("m", "b", "p", "t","w", "j", "h", "c", "i", "k"), labels = c("McDonalds", "Burger King", "Pizza Hut", "Taco Bell", "Wendys", "Jack In The Box", "Hardees", "Carls Jr.", "In-N-Out", "KFC")) 
 
 # This selects only the zip code, median salary, and restaurant columns
-Plot1_df <- Plot1_df %>% select(ZIP, MEDIAN, RESTAURANT, POP)
+Plot2_df <- Plot2_df %>% select(ZIP, MEDIAN, RESTAURANT, POP)
 
 # This shows data for zip codes that have median salaries that are either in the top or bottom ten percent 
-Plot1_df <- Plot1_df  %>% mutate(MEDIAN_SAL_PERCENT = cume_dist(Plot1_df$MEDIAN)) %>% filter(MEDIAN_SAL_PERCENT  <= .1 | MEDIAN_SAL_PERCENT  >= .9)
+Plot2_df <- Plot2_df  %>% mutate(MEDIAN_SAL_PERCENT = cume_dist(Plot2_df$MEDIAN)) %>% filter(MEDIAN_SAL_PERCENT  <= .1 | MEDIAN_SAL_PERCENT  >= .9)
 
 # Main workhorse function. This is what took 4 hours to figure out
-Plot1_df <- Plot1_df %>%group_by(ZIP,POP) %>% summarize(TOTAL_RESTAURANTS = n())%>%ungroup()%>%arrange(POP)
+Plot2_df <- Plot2_df %>%group_by(ZIP,POP) %>% summarize(TOTAL_RESTAURANTS = n())%>%ungroup()%>%arrange(POP)
 
 ggplot() +
   coord_cartesian() + 
@@ -24,7 +24,7 @@ ggplot() +
   #facet_wrap(~R) +
   labs(title="The relationship between the population and the total number of fast food restaurants") +
   labs(x="Population", y="Number of Restaurants", color="Fast Food restaurant") +
-  layer(data=Plot1_df , 
+  layer(data=Plot2_df , 
         mapping=aes(x=as.numeric(POP), y=as.numeric(TOTAL_RESTAURANTS)),
         stat="identity",
         stat_params=list(), 
--- 02 Data Wrangling/Project3_Plot3.R
+++ 02 Data Wrangling/Project3_Plot3.R
@@ -3,19 +3,19 @@ require(tidyr)
 require(dplyr)
 require(ggplot2)
 # Join the fast food and zip code data into new data frame for Plot 1
-Plot1_df <- dplyr::left_join(fast_food, zip_code, by="ZIP") 
+Plot3_df <- dplyr::left_join(fast_food, zip_code, by="ZIP") 
 
 # Need to relabel the data in the RESTAURANT column
-Plot1_df$RESTAURANT <- factor(Plot1_df$RESTAURANT, levels=c("m", "b", "p", "t","w", "j", "h", "c", "i", "k"), labels = c("McDonalds", "Burger King", "Pizza Hut", "Taco Bell", "Wendys", "Jack In The Box", "Hardees", "Carls Jr.", "In-N-Out", "KFC")) 
+Plot3_df$RESTAURANT <- factor(Plot3_df$RESTAURANT, levels=c("m", "b", "p", "t","w", "j", "h", "c", "i", "k"), labels = c("McDonalds", "Burger King", "Pizza Hut", "Taco Bell", "Wendys", "Jack In The Box", "Hardees", "Carls Jr.", "In-N-Out", "KFC")) 
 
 # This selects only the zip code, median salary, and restaurant columns
-Plot1_df <- Plot1_df %>% select(ZIP, MEDIAN, RESTAURANT, POP)
+Plot3_df <- Plot3_df %>% select(ZIP, MEDIAN, RESTAURANT, POP)
 
 # This shows data for zip codes that have median salaries that are either in the top or bottom ten percent 
-Plot1_df <- Plot1_df  %>% mutate(POP_PERCENT = cume_dist(Plot1_df$POP)) %>% filter(POP_PERCENT <= .1 | POP_PERCENT >= .9)
+Plot3_df <- Plot3_df  %>% mutate(POP_PERCENT = cume_dist(Plot3_df$POP)) %>% filter(POP_PERCENT <= .1 | POP_PERCENT >= .9)
 
 # Main workhorse function. This is what took 4 hours to figure out
-Plot1_df <- Plot1_df %>% group_by(ZIP) %>% summarize(POP = first(POP), TOTAL_RESTAURANTS = n())
+Plot3_df <- Plot3_df %>% group_by(ZIP) %>% summarize(POP = first(POP), TOTAL_RESTAURANTS = n())
 
 
 ggplot() +
@@ -25,7 +25,7 @@ ggplot() +
   #facet_wrap(~R) +
   labs(title="Top & Bottom 10 percent of Zip codes by population") +
   labs(x="Population", y="Number of Restaurants", color="Fast Food restaurant") +
-  layer(data=Plot1_df , 
+  layer(data=Plot3_df , 
         mapping=aes(x=as.numeric(POP), y=as.numeric(TOTAL_RESTAURANTS)),
         stat="identity",
         stat_params=list(), 
